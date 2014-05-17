c65448a19949ae21fdb63da6593c632ef2450d5e
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2014-05-17 19:48:55 +0200

    example of image generation

--- src/main/java/ViewerApp.java
+++ src/main/java/ViewerApp.java
@@ -1,9 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.io.*;
-import java.net.URL;
-import javax.imageio.*;
 import javax.swing.*;
 
 /**
@@ -18,26 +15,22 @@ public class ViewerApp extends Component {
     }
 
     public ViewerApp() {
-        try {
-            URL stream = getClass().getResource("tiger.jpg");
-            img = ImageIO.read(stream);
-        } catch (IOException e) {
-            System.out.println(e);
+        img = new BufferedImage(800,400,BufferedImage.TYPE_INT_RGB);
+        for(int i=0;i<800;i++) {
+            for(int j=0;j<400;j++) {
+                Color randomColor = new Color((int)(Math.random() * 0x1000000));
+                img.setRGB(i, j, randomColor.getRGB());
+            }
         }
-
     }
 
     public Dimension getPreferredSize() {
-        if (img == null) {
-            return new Dimension(100,100);
-        } else {
-            return new Dimension(img.getWidth(null), img.getHeight(null));
-        }
+        return new Dimension(img.getWidth(null), img.getHeight(null));
     }
 
     public static void main(String[] args) {
 
-        JFrame f = new JFrame("Load Image Sample");
+        JFrame f = new JFrame("MandelBrot");
 
         f.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent e) {
